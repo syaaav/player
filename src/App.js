387375d8286824d2adbc8ea141b7c720a0e8e7f6
syaaav <mysyava@gmail.com>
2022-08-12 15:20:48 +0300

    Create 404 error page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LogIn from './components/pages/log-in/log-in'
 import SignIn from './components/pages/sign-in/sign-in'
 import Main from './components/pages/main/main'
+import Page404 from './components/pages/page404/page404'
 import './App.css'
 
 const App = () => (
   <div className="wrapper">
     <Routes>
-      <Route path="/" element={<LogIn />} />
-      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/user/login/" element={<LogIn />} />
+      <Route path="/user/signup/" element={<SignIn />} />
       <Route path="/catalog/track/all/" element={<Main />} />
-      {/* <Route path="/cart/:quantity/:price" element={<GeneralCart />} />
-      <Route path="*" element={<Page404 />} /> */}
+      {/* <Route path="/cart/:quantity/:price" element={<GeneralCart />} /> */}
+      <Route path="*" element={<Page404 />} />
     </Routes>
   </div>
 )
---
+++ src/components/pages/page404/page404.jsx
@@ -0,0 +1,10 @@
+import '../../../css/style.css'
+
+const Page404 = () => (
+  <div className="container page404">
+    <span className="page404__img">😢</span>
+    <span className="page404__title">Page not found</span>
+  </div>
+)
+
+export default Page404

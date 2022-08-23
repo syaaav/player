f22c5d2dbe192095b5e1de83477ae35f40fd0317
syaaav <mysyava@gmail.com>
2022-08-23 19:32:20 +0300

    Add hide menu function

--- src/components/pages/main/main.jsx
+++ src/components/pages/main/main.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import logo from '../../../img/logo.png'
 import Menu from './menu'
 import Bar from './bar'
@@ -5,27 +6,43 @@ import CenterBlock from './centerblock'
 import SideBar from './sidebar'
 import '../../../css/style.css'
 
-const Main = () => (
-  <div className="wrapper">
-    <div className="container">
-      <main className="main">
-        <nav className="main__nav nav">
-          <div className="nav__logo logo">
-            <img className="logo__image" src={logo} alt="logo" />
-          </div>
-          <div className="nav__burger burger">
-            <span className="burger__line" />
-            <span className="burger__line" />
-            <span className="burger__line" />
-          </div>
-          <Menu className="hidden" />
-        </nav>
-        <CenterBlock />
-        <SideBar />
-      </main>
-      <Bar />
+const Main = () => {
+  const [isHidden, setHidden] = useState(false)
+
+  function handleHidden() {
+    setHidden((prev) => !prev)
+  }
+
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <main className="main">
+          <nav className="main__nav nav">
+            <div className="nav__logo logo">
+              <img className="logo__image" src={logo} alt="logo" />
+            </div>
+            <div
+              className="nav__burger burger"
+              role="button"
+              onClick={handleHidden}
+              onKeyDown={handleHidden}
+              tabIndex={0}
+            >
+              <span className="burger__line" />
+              <span className="burger__line" />
+              <span className="burger__line" />
+            </div>
+            <div className={`nav__menu menu ${!isHidden ? 'hidden' : ''}`}>
+              <Menu />
+            </div>
+          </nav>
+          <CenterBlock />
+          <SideBar />
+        </main>
+        <Bar />
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Main
--- src/components/pages/main/menu.jsx
+++ src/components/pages/main/menu.jsx
@@ -1,25 +1,23 @@
 import '../../../css/style.css'
 
 const Menu = () => (
-  <div className="nav__menu menu">
-    <ul className="menu__list">
-      <li className="menu__item">
-        <a href="http://" className="menu__link">
-          Главное
-        </a>
-      </li>
-      <li className="menu__item">
-        <a href="http://" className="menu__link">
-          Мой плейлист
-        </a>
-      </li>
-      <li className="menu__item">
-        <a href="http://" className="menu__link">
-          Войти
-        </a>
-      </li>
-    </ul>
-  </div>
+  <ul className="menu__list">
+    <li className="menu__item">
+      <a href="http://" className="menu__link">
+        Главное
+      </a>
+    </li>
+    <li className="menu__item">
+      <a href="http://" className="menu__link">
+        Мой плейлист
+      </a>
+    </li>
+    <li className="menu__item">
+      <a href="http://" className="menu__link">
+        Войти
+      </a>
+    </li>
+  </ul>
 )
 
 export default Menu
